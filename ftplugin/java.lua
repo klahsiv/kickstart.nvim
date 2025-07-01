667b3dbc04b9e9471d435dd4a842bee4a3b1b860
klahsiv <vishalkumar2803001@gmail.com>
2025-07-02 02:09:41 +0530

    Added Java LSP

---
+++ ftplugin/java.lua
@@ -0,0 +1,29 @@
+local home = os.getenv 'HOME'
+local jdtls_path = home .. '/.local/share/nvim/mason/packages/jdtls'
+
+local config = {
+  cmd = {
+    -- Use Java 21 explicitly
+    '/usr/lib/jvm/java-21-openjdk-amd64/bin/java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xms1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-jar',
+    vim.fn.glob(jdtls_path .. '/plugins/org.eclipse.equinox.launcher_*.jar'),
+    '-configuration',
+    jdtls_path .. '/config_linux',
+    '-data',
+    home .. '/.cache/jdtls/workspace/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t'),
+  },
+  root_dir = require('jdtls.setup').find_root { 'gradlew', '.git', 'mvnw' },
+}
+
+require('jdtls').start_or_attach(config)
--- init.lua
+++ init.lua
@@ -475,6 +475,7 @@ require('lazy').setup({
       },
     },
   },
+  { 'mfussenegger/nvim-jdtls' },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -671,8 +672,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+        clangd = {},
+        gopls = {},
+        jdtls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
