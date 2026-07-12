ceb8817ac65794bc25c5e206749921c3cf7d8431
klahsiv <vishalkumar2803001@gmail.com>
2026-07-12 13:05:01 +0530

    Auto read to refresh buffer

--- init.lua
+++ init.lua
@@ -225,6 +225,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+--
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold' }, {
+  command = 'silent! checktime',
+})
+
+vim.o.autoread = true
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
